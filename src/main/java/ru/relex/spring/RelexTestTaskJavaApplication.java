bbacd7d48c1696834b2fc03b7f2ef48ac6784233
Николай Шведов <shved.nikolay.18@mail.ru>
2022-10-20 22:22:18 +0300

    init project

---
+++ src/main/java/ru/relex/spring/RelexTestTaskJavaApplication.java
@@ -0,0 +1,13 @@
+package ru.relex.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelexTestTaskJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelexTestTaskJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/relex/spring/RelexTestTaskJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.relex.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelexTestTaskJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
